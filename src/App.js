6bed23661533e2cf563b9f8a8b01664c1bbe8044
advaya <advayayaya@gmail.com>
2026-01-13 22:50:10 +0530

    class components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Greet} from './components/Greet.js' //for default export {Greet} and <Greet / >
-//import MyComponent from './components/Greet.js' //default export
+import Welcome from './components/Welcome';
 
 class App extends Component {
   render() {
     return (
     <div className = "App">
-      <Greet /> 
-      {/* <MyComponent /> default export */}
+      <Welcome /> 
     </div>
     );
   }
