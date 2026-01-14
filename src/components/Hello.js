d5ba1732d87370253dd7a2719b5d120cf664bce1
advaya <advayayaya@gmail.com>
2026-01-14 14:58:41 +0530

    Without jsx version of Hello component

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const Hello = () => {
-    return (
-        <div>
-            <h1>Hey adwaya</h1>
-        </div>
+    // return (
+    //     <div className="dummyclass">
+    //         <h1>Hey adwaya</h1>
+    //     </div>
+    // )
+
+    return React.createElement(
+        'div', //html tag
+        {id: "hello", className: "dummyclass"}, //optional properties, obj of key val pair that will be applied to the element
+        //in JS 'class' is a reserved word, we use class keyword to create react components, so in react a css class has to be specified using className attribute
+        React.createElement('h1', null, 'hello adwaya') //children for div tag
     )
 }
 
