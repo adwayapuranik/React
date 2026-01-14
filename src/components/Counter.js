2c172a61b14a5e7e497f7063a395ca3fbe893467
advaya <advayayaya@gmail.com>
2026-01-14 20:02:50 +0530

    to update new state value using current state value, pass funcn as an argument instead of obj

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -13,21 +13,39 @@ class Counter extends Component {
 increment() {
     // this.state.count = this.state.count + 1 doesnt change count in UI doesnt get rendered, dont modify the state directly
     //
-    this.setState({
-        count: this.state.count + 1
-    }, 
-    () => {
-        console.log('Callback value', this.state.count);
-    })
-    console.log(this.state.count);
+    // this.setState({
+    //     count: this.state.count + 1
+    // }, 
+    // () => {
+    //     console.log('Callback value', this.state.count);
+    // })
+    // console.log(this.state.count);
+
+    this.setState((prevState) => ({
+        count: prevState.count + 1
+    }) )
+
+    //  this.setState((prevState,props) => ({
+    //     count: prevState.count + props.addValue
+    // }) )
 }
+
+
+incrementFive() {
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+}
+
 
   render() {
     return (
         <div>
 
         <div>count - {this.state.count}</div>
-        <button onClick={() => this.increment()}>Increment </button>
+        <button onClick={() => this.incrementFive()}>Increment </button>
 
         </div>
     )
