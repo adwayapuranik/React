5e75205f72b4219261f8a92e314ceea6ee44c642
advaya <advayayaya@gmail.com>
2026-01-14 14:41:30 +0530

    jsx version of Hello component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 class App extends Component {
   render() {
     return (
     <div className = "App">
-      <Welcome /> 
+      {/* <Welcome />  */}
+      <Hello />
     </div>
     );
   }
---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hey adwaya</h1>
+        </div>
+    )
+}
+
+export default Hello;
